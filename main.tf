eeeb1c19e70e4bc352af97fce2e6b799484fdecc
lenmg <lenmg@hotmail.com>
2020-11-04 15:04:11 -0800

    Modified main

--- main.tf
+++ main.tf
@@ -14,3 +14,14 @@ tags = {
     Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
